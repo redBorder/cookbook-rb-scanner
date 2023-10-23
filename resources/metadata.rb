e0a17bbee6e965372b3d7b2c0552f47f872a8be4
Luis Blanco <ljblanco@redborder.com>
2023-10-23 13:43:06 +0100

    fix curls

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'javiercrg@redborder.com'
 license          'All rights reserved'
 description      'Installs/Configures redborder scanner'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
